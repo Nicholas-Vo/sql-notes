17472a3478d1f93dcc334c2577e4db80cc9d5b23
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2023-04-03 13:47:17 -0500

    Update insert-update-commit-rollback.sql

--- insert-update-commit-rollback.sql
+++ insert-update-commit-rollback.sql
@@ -35,6 +35,38 @@ WHERE employee_id = 1002;
 
 ROLLBACK TO salary_change;
 
+/* All of the above commands demonstrated at once: */
+
+-- Begin transaction
+START TRANSACTION;
+
+-- Insert a new row into the "employees" table
+INSERT INTO employees (employee_id, first_name, last_name, email, hire_date, job_id, salary)
+VALUES (1003, 'Jane', 'Doe', 'jane@example.com', TO_DATE('2023-04-03', 'YYYY-MM-DD'), 'IT_PROG', 5000);
+
+-- Update the "salary" column for an employee
+UPDATE employees
+SET salary = 5500
+WHERE employee_id = 1001;
+
+-- Create a savepoint for the transaction
+SAVEPOINT my_savepoint;
+
+-- Update the "salary" column for another employee
+UPDATE employees
+SET salary = 6000
+WHERE employee_id = 1002;
+
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Commit the changes
+COMMIT;
+
+-- Query the "employees" table to verify the changes
+SELECT * FROM employees;
+
+
 
  
  
